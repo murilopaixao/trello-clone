79f700bece5a8cf4a166a80be7652cd5f3230fc5
murilopaixao <murilopaixao@gmail.com>
2023-05-18 10:45:29 -0300

    listas

--- views.py
+++ views.py
@@ -3,7 +3,49 @@ from app import app
 import json
 import datetime
 from os import getenv
+from tinydb import TinyDB, Query
+
+
+boardDB = TinyDB('./db/board.json')
+listasDB = TinyDB('./db/listas.json')
+cartoes = TinyDB('./db/cartoes.json')
+q = Query()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    listaBoard = boardDB.all()
+    return render_template('index.html', listaBoard=listaBoard)
+
+@app.route('/newboard')
+def newboard():
+    cod = "003"
+    board = "Valle"
+    myDoc = {
+        "cod": cod,
+        "board": board
+    }
+    boardDB.insert(myDoc)
+    return "Ok"
+
+@app.route('/b/<board>')
+def b(board):
+    boards = boardDB.all()
+    listas = listasDB.all()
+    boardAtual = boardDB.search(q.cod == board)
+    return render_template('board.html', board=board, boardAtual=boardAtual, listas=listas, boards=boards)
+
+@app.route('/newlist')
+def newlist():
+    board = "001"
+    codLista = "l003"
+    nomeLista = "done"
+    posicao = 3
+    myDoc = {
+        "codLista": codLista,
+        "board": board,
+        "posicao": posicao,
+        "nomeLista": nomeLista
+    }
+    listasDB.insert(myDoc)
+    return "Ok"
+
